dc9bdeef37547b0bc0332513798350ebdcd9ac5b
Idogun John <sirneij@gmail.com>
2021-10-21 18:34:43 +0100

    recipe data model

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type teamSchema struct {
+	teamName    []string  `json:"teamName"`
+	teamMembers []string  `json:"teamMembers"`
+	description string    `json:"description"`
+	createdAt   time.Time `json:"createdAt"`
+	updatedAt   time.Time `json:"updatedAt"`
+}
 
 func main() {
 	router := gin.Default()
